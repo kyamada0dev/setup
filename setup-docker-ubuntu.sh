576ace0ca1061337e89795ea734b12510c366c9f
Kenji Yamada <kenji.yamada@gmail.com>
2026-01-28 07:20:54 +0900

    update: change docker install procedure for Ubuntu

--- setup-docker-ubuntu.sh
+++ setup-docker-ubuntu.sh
@@ -1,22 +1,10 @@
 #!/bin/sh
 
 echo 'Setup docker'
-# Add Docker's official GPG key:
-# sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg lsb-release
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] \
-  https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+curl -fsSL https://get.docker.com | sudo sh
 
 sudo usermod -aG docker "$USER"
 echo 'exec "newgrp docker"'
